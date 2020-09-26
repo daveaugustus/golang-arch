05b1e6586169c553e6fc67b58b9f263530abb180
Dave Augustus <davetweetlive@gmail.com>
2020-09-26 14:33:38 +0530

    Unmarshal

--- main.go
+++ main.go
@@ -21,9 +21,19 @@ func main() {
 
 	xp := []person{p1, p2}
 
+	fmt.Println("Marshaling")
 	jsonbyte, err := json.Marshal(xp)
 	if err != nil {
 		log.Panic(err)
 	}
 	fmt.Println(string(jsonbyte))
+
+	// Unmarshaling the same
+
+	fmt.Println("Unmarshaling")
+	xp2 := []person{}
+	if err := json.Unmarshal(jsonbyte, &xp2); err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(xp2)
 }
