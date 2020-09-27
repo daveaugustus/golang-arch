34498f3c29f4ebe33e48777c04c5546f2da59c59
Dave Augustus <davetweetlive@gmail.com>
2020-09-27 20:44:58 +0530

    Create claims

---
+++ 03_JWT/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// UserClaims as a
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+	if u.SessionID == 0 {
+		return fmt.Errorf("Invalid Session Id")
+	}
+}
+
+var key = []byte{}
+
+func main() {
+
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
+
+	pass := "123456789"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Hash: ", hashedPass)
+
+	if err := comparePassword(pass, hashedPass); err != nil {
+		log.Fatalln("Incorrect password")
+	}
+
+	fmt.Println("Logged in")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+}
+
+func comparePassword(password string, hashedPassword []byte) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("Error while hashing msgs: %w", err)
+	}
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+func checkSignature(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSig: %w", err)
+	}
+	return hmac.Equal(newSig, sig), nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/davetweetlive/golang-arch
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+)
